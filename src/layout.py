d5b87cc69bc57b11b9b83eb1d72dd1b22d117138
yattom <tsutomu.yasui@gmail.com>
2011-07-14 13:01:28 +0900

    tweaked story layout

--- src/layout.py
+++ src/layout.py
@@ -23,30 +23,44 @@ class FillLayout(object):
     def stabilize(self, max_trial=100, minimum=1.0):
         for i in xrange(max_trial):
             d = self.shift_apart()
-            if d < minimum:
+            if d <= minimum:
                 return
 
     def shift_apart(self):
         total_shift_dist = 0.0
         for s in self.shapes:
-            fx = fy = 0
+            fx = fy = 0.0
+
+            # apply strong forth not to be within rect
             if s.x - s.radius < 0:
                 fx += -(s.x - s.radius)
-            if self.width <= s.x + s.radius:
+            if self.width < s.x + s.radius:
                 fx += self.width - (s.x + s.radius)
             if s.y - s.radius < 0:
                 fy += -(s.y - s.radius)
-            if self.height <= s.y + s.radius:
+            if self.height < s.y + s.radius:
                 fy += self.height - (s.y + s.radius)
 
+            # apply weak forth not to be within rect
+            if 0 < s.x - s.radius < s.radius:
+                fx += -(s.x - 2 * s.radius) / 8.0
+            if self.width - s.radius < s.x + s.radius < self.width:
+                fx += (self.width - (s.x + 2 * s.radius)) / 8.0
+            if 0 < s.y - s.radius < s.radius:
+                fy += -(s.y - 2 * s.radius) / 8.0
+            if self.height - s.radius < s.y + s.radius < self.height:
+                fy += (self.height - (s.y + 2 * s.radius)) / 8.0
+
+            # apply mid forth to separate with each other
             for s2 in self.shapes:
                 if s == s2: continue
-                d = math.sqrt((s.x - s2.x) ** 2 + (s.y - s2.y) ** 2)
-                if d >= s.radius + s2.radius: continue
-                f = ((s.radius + s2.radius) - d) / (s.radius + s2.radius)
-                fx += (s.x - s2.x) * f
-                fy += (s.y - s2.y) * f
-            s.x += fx / 4.0
-            s.y += fy / 4.0
+                dist = math.sqrt((s.x - s2.x) ** 2 + (s.y - s2.y) ** 2)
+                minimum_dist = (s.radius + s2.radius) * 1.2
+                if dist >= minimum_dist: continue
+                f = ((minimum_dist) - dist) / (minimum_dist)
+                fx += (s.x - s2.x) * f / 4.0
+                fy += (s.y - s2.y) * f / 4.0
+            s.x += fx
+            s.y += fy
             total_shift_dist += abs(fx) + abs(fy)
         return total_shift_dist
--- src/layout_test.py
+++ src/layout_test.py
@@ -18,33 +18,34 @@ class FillLayoutTest(unittest.TestCase):
             layout = FillLayout(width=100, height=100)
             r = 10
             layout.add_circle('key!', r)
-            layout.stabilize()
+            layout.stabilize(minimum=0.0)
 
             self.assertEquals(1, len(layout.shapes))
             self.assertEqual(layout.shapes[0].key, 'key!')
             self.assertEqual(layout.shapes[0].radius, r)
-            self.assertTrue((0 + r) <= layout.shapes[0].x <= (100 - r))
-            self.assertTrue((0 + r) <= layout.shapes[0].y <= (100 - r))
+            self.assertTrue((0) <= layout.shapes[0].x <= (100))
+            self.assertTrue((0) <= layout.shapes[0].y <= (100))
 
     def test_fill_multi(self):
-        layout = FillLayout(width=100, height=100)
+        layout = FillLayout(width=200, height=200)
         r = 10
+        epsilon = 2.0
         count = 10
         for i in range(count):
             layout.add_circle('key%02d'%(i), r + i)
-        layout.stabilize()
+        layout.stabilize(minimum=0.0)
 
         self.assertEquals(count, len(layout.shapes))
         for i, s in enumerate(layout.shapes):
             self.assertEqual(s.key, 'key%02d'%(i))
             self.assertEqual(s.radius, r + i)
-            self.assertTrue((0 + r + i) <= s.x <= (100 - (r + i)))
-            self.assertTrue((0 + r + i) <= s.y <= (100 - (r + i)))
+            self.assertTrue((0 + s.radius - epsilon) <= s.x <= (200 - s.radius + epsilon))
+            self.assertTrue((0 + s.radius - epsilon) <= s.y <= (200 - s.radius + epsilon))
 
             for s2 in layout.shapes:
                 if s == s2: continue
                 d = math.sqrt(abs(s.x - s2.x) ** 2 + abs(s.y - s2.y) ** 2)
-                self.assertTrue(d >= s.radius + s2.radius)
+                self.assertTrue(d + epsilon >= s.radius + s2.radius)
 
 
 
