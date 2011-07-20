a3b4420f8847d975e279c2c54060bacb4c70148a
yattom <tsutomu.yasui@gmail.com>
2011-07-20 09:57:23 +0900

    refactored a bit and moved the iteration boxes

--- src/visual.py
+++ src/visual.py
@@ -5,20 +5,31 @@ import random
 import time
 
 from layout import FillLayout
+
 class Visual(object):
+    BOX_FRAME_COLOR = 255, 255, 255
+    BOX_FILL_COLOR = 0, 0, 0
+
     def start(self):
         pygame.init()
-        self.screen = pygame.display.set_mode((300, 600))
+        self.screen = pygame.display.set_mode((500, 800))
         self.drawings = {}
 
+        self.iteration_area = pygame.Rect(100, 100, 300, 600)
+
     def draw_iteration_boxes(self, iteration_count):
-        white = 255, 255, 255
-        black = 0, 0, 0
+        Visual.BOX_FRAME_COLOR = 255, 255, 255
+        Visual.BOX_FILL_COLOR = 0, 0, 0
         self.iteration_count = iteration_count
-        self.iteration_height = 600 / iteration_count
+        self.iteration_height = self.iteration_area.height / iteration_count
         for it in range(iteration_count):
-            pygame.draw.rect(self.screen, white, ((0, it * self.iteration_height), (299, (it + 1) * self.iteration_height)))
-            pygame.draw.rect(self.screen, black, ((1, it * self.iteration_height + 1), (298, (it + 1) * self.iteration_height - 1)))
+            frame_rect = pygame.Rect(self.iteration_area.left,
+                                     self.iteration_area.top + it * self.iteration_height,
+                                     self.iteration_area.width - 1,
+                                     self.iteration_height)
+            inner_rect = pygame.Rect(frame_rect.left + 1, frame_rect.top + 1, frame_rect.width - 2, frame_rect.height - 2)
+            pygame.draw.rect(self.screen, Visual.BOX_FRAME_COLOR, frame_rect)
+            pygame.draw.rect(self.screen, Visual.BOX_FILL_COLOR, inner_rect)
         pygame.display.flip()
 
     def draw_stories_for_iteration(self, stories, iteration):
@@ -39,10 +50,13 @@ class Visual(object):
             sprite = pygame.sprite.DirtySprite(group)
             sprite.image = pygame.Surface((shape.radius * 2, shape.radius * 2), flags=pygame.SRCALPHA)
             sprite.image.fill((0, 0, 0, 0))
-            sprite.rect = ((int(shape.x - shape.radius), int(shape.y  - shape.radius+ self.iteration_height * iteration)), (int(shape.x + shape.radius), int(shape.y + shape.radius + self.iteration_height * iteration)))
+            sprite.rect = pygame.Rect(int(shape.x - shape.radius),
+                                      int(shape.y  - shape.radius + self.iteration_height * iteration),
+                                      int(shape.x + shape.radius),
+                                      int(shape.y + shape.radius + self.iteration_height * iteration))
+            sprite.rect.left += self.iteration_area.left
+            sprite.rect.top += self.iteration_area.top
             pygame.draw.circle(sprite.image, green, (shape.radius, shape.radius), int(shape.radius), 1)
-
-            #pygame.draw.circle(self.screen, green, (int(shape.x), int(shape.y) + self.iteration_height * iteration), int(shape.radius), 1)
 
         group.draw(self.screen)
         pygame.display.flip()
